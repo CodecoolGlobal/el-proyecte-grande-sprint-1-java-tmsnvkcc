ef1b498abe9e82d940be549f0c7ce0167a32d033
Erik Csepeti <Erik-EEI@users.noreply.github.com>
2023-11-14 14:33:09 +0100

    Implement conditional rendering of components to track page

--- frontend/src/pages/Track/Track.page.jsx
+++ frontend/src/pages/Track/Track.page.jsx
@@ -1,11 +1,32 @@
 import { useState } from 'react';
 import './Track.styles.css';
-import {TrackComponentSelector} from "../../components/track/index.js";
+import { TrackComponentSelector } from '../../components/track/index.js';gi
 
 const Track = () => {
   const [currentTile, setCurrentTile] = useState('Overview');
 
-  const handleClick = ( tileName ) => { setCurrentTile(tileName)};
+  const componentRenderHandler = () => {
+    switch (currentTile) {
+    case 'Overview':
+      return 'Overview Component Placeholder';
+
+    case 'Spendings':
+      return 'Spendings Component Placeholder';
+
+    case 'Income':
+      return 'Income Component Placeholder';
+
+    case 'Savings':
+      return 'Savings Component Placeholder';
+
+    default:
+      return 'Error';
+    }
+  };
+
+  const handleClick = (tileName) => {
+    setCurrentTile(tileName);
+  };
 
   return (
     <div>
@@ -15,7 +36,7 @@ const Track = () => {
         <TrackComponentSelector buttonLabel='Savings' clickHandler={handleClick}/>
         <TrackComponentSelector buttonLabel='Overview' clickHandler={handleClick}/>
       </div>
-
+      {componentRenderHandler()}
     </div>
   );
 };
