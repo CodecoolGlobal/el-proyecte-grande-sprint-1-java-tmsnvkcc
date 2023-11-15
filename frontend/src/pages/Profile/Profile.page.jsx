ef8051bc7fa431f54bea541ea05c6006bac9dd87
Balogh Barnabas <111227723+baloghb0110@users.noreply.github.com>
2023-11-15 14:24:50 +0100

    fetching profile data

--- frontend/src/pages/Profile/Profile.page.jsx
+++ frontend/src/pages/Profile/Profile.page.jsx
@@ -1,7 +1,33 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+
 const Profile = () =>{
-  return(
+  const [loading, setLoading] = useState(false);
+  const [profileData, setProfileData] = useState(null);
+
+  if (loading) {
+    return 'load data';
+  }
+
+  useEffect(() => {
+    const fetchData = async() =>{
+      await axios.get('/api/user/')
+        .then((response) => {
+          setProfileData(response.data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+
+    fetchData();
+  }, []);
+
+  return (
     <>
-      profile page
+      {profileData}
     </>
   )
 };
