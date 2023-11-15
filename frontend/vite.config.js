58cbabd52226a9025455625141d89e6b5754e0a1
Balogh Barnabas <111227723+baloghb0110@users.noreply.github.com>
2023-11-15 14:17:52 +0100

    fixing proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,26 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'localhost:8080',
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
       },
     },
   },
   resolve: {
     alias: {
-      src: '/src',
-      assets: '/src/assets',
-      config: '/src/config',
-      components: '/src/components',
-      pages: '/src/pages',
-      hooks: '/src/hooks',
-      utilities: '/src/utilities',
+      src: "/src",
+      assets: "/src/assets",
+      config: "/src/config",
+      components: "/src/components",
+      pages: "/src/pages",
+      hooks: "/src/hooks",
+      utilities: "/src/utilities",
     },
   },
 });
