ddc6359ec483f103dc0367dde9bca232d21652a4
Balogh Barnabas <111227723+baloghb0110@users.noreply.github.com>
2023-11-15 14:32:07 +0100

    implementing /api/user/ GET endpoint

--- backend/src/main/java/com/codecool/controller/user/UserController.java
+++ backend/src/main/java/com/codecool/controller/user/UserController.java
@@ -1,7 +1,25 @@
 package com.codecool.controller.user;
 
-import org.springframework.stereotype.Controller;
+import com.codecool.postgresDb.PsqlConnectorImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
+    private static final Logger logger = LoggerFactory.getLogger(PsqlConnectorImpl.class);
+    @GetMapping("/")
+    public ResponseEntity<String> getUser(){
+        try{
+            return new ResponseEntity<>(HttpStatus.OK);
+        }catch(Exception e){
+            logger.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+        }
+    }
 }
