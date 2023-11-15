568a517e4b12c00f05aa13a414ee825f2d17c734
Andras Toth <125831134+andrastoth021@users.noreply.github.com>
2023-11-15 13:33:07 +0100

    implement dashboard page

---
+++ frontend/src/pages/Dashboard/Dashboard.page.jsx
@@ -0,0 +1,15 @@
+import {
+    // components
+} from 'components/home';
+import './Dashboard.styles.css';
+
+const Dashboard = () => {
+
+  return (
+    <>
+      <h1>Dashboard page!</h1>
+    </>
+  );
+};
+
+export default Dashboard;
