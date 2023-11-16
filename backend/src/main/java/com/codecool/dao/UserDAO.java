0834ae5835244e731221cd283efaa1eeebc54ef2
Balogh Barnabas <111227723+baloghb0110@users.noreply.github.com>
2023-11-16 11:02:25 +0100

    implementing updates methods

--- backend/src/main/java/com/codecool/dao/UserDAO.java
+++ backend/src/main/java/com/codecool/dao/UserDAO.java
@@ -6,9 +6,9 @@ import java.util.UUID;
 
 public interface UserDAO {
     void deleteUser(UUID uuid);
-    void updatePassword(UUID uuid);
-    void updateEmail(UUID uuid);
-    void updateUsername(UUID uuid);
+    void updatePassword(UUID uuid, String newPassword);
+    void updateEmail(UUID uuid, String newEmail);
+    void updateUsername(UUID uuid, String newUsername);
     User getUser(String email, String password);
     void addUser(String email, String username, boolean isAdmin);
 }
--- backend/src/main/java/com/codecool/dao/UserDAOImpl.java
+++ backend/src/main/java/com/codecool/dao/UserDAOImpl.java
@@ -2,13 +2,18 @@ package com.codecool.dao;
 
 import com.codecool.dao.model.User;
 import com.codecool.postgresDb.PsqlConnector;
+import com.codecool.postgresDb.PsqlConnectorImpl;
 import org.springframework.stereotype.Repository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.sql.Connection;
+import java.sql.*;
+import java.sql.PreparedStatement;
 import java.util.UUID;
 
 @Repository
 public class UserDAOImpl implements UserDAO{
+    private static final Logger logger = LoggerFactory.getLogger(PsqlConnectorImpl.class);
     private final PsqlConnector psqlConnector;
 
     public UserDAOImpl(PsqlConnector psqlConnector) {
@@ -25,18 +30,57 @@ public class UserDAOImpl implements UserDAO{
     }
 
     @Override
-    public void updatePassword(UUID uuid) {
+    public void updatePassword(UUID uuid, String newPassword) {
+        String query = "UPDATE users SET password = ? WHERE uuid = ?";
 
+        try (Connection conn = getPsqlConnector()) {
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setString(1, newPassword);
+            preparedStatement.setString(2, uuid.toString());
+
+            preparedStatement.executeUpdate();
+
+            logger.info("Updating user's password was successfully");
+
+        } catch (SQLException e) {
+            logger.error("Error updating user: " + e.getMessage());
+        }
     }
 
     @Override
-    public void updateEmail(UUID uuid) {
+    public void updateEmail(UUID uuid, String newEmail) {
+        String query = "UPDATE users SET username = ? WHERE uuid = ?";
 
+        try (Connection conn = getPsqlConnector()) {
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setString(1, newEmail);
+            preparedStatement.setString(2, uuid.toString());
+
+            preparedStatement.executeUpdate();
+
+            logger.info("Updating user's email was successfully");
+
+        } catch (SQLException e) {
+            logger.error("Error updating user: " + e.getMessage());
+        }
     }
 
     @Override
-    public void updateUsername(UUID uuid) {
+    public void updateUsername(UUID uuid, String newUsername) {
+        String query = "UPDATE users SET username = ? WHERE uuid = ?";
 
+        try (Connection conn = getPsqlConnector()) {
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setString(1, newUsername);
+            preparedStatement.setString(2, uuid.toString());
+
+            preparedStatement.executeUpdate();
+
+            logger.info("Updating user's username was successfully");
+
+        } catch (SQLException e) {
+            logger.error("Error updating user: " + e.getMessage());
+        }
     }
 
     @Override
