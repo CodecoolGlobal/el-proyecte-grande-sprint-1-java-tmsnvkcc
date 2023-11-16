8bc86f1c361fb8df820d513f7fb3b9436952c2f6
Balogh Barnabas <111227723+baloghb0110@users.noreply.github.com>
2023-11-16 09:32:41 +0100

    implementing user profile page endpoints

--- backend/src/main/java/com/codecool/controller/user/UserController.java
+++ backend/src/main/java/com/codecool/controller/user/UserController.java
@@ -5,19 +5,51 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
+    // TODO: delete userId from all endpoint because it will be in token
+
     private static final Logger logger = LoggerFactory.getLogger(PsqlConnectorImpl.class);
     @GetMapping("/")
     public ResponseEntity<String> getUser(){
         try{
             return new ResponseEntity<>(HttpStatus.OK);
         }catch(Exception e){
+            logger.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+        }
+    }
+
+    @PutMapping("/changeUsername/{userId}")
+    public ResponseEntity<String> changeUsername(@PathVariable UUID userId){
+        try{
+            return new ResponseEntity<>(HttpStatus.OK);
+        }catch (Exception e){
+            logger.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+        }
+    }
+
+    @PutMapping("/changePassword/{userId}")
+    public ResponseEntity<String> changePassword(@PathVariable UUID userId){
+        try{
+            return new ResponseEntity<>(HttpStatus.OK);
+        }catch (Exception e){
+            logger.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+        }
+    }
+
+    @PutMapping("/changeEmail/{userId}")
+    public ResponseEntity<String> changeEmail(@PathVariable UUID userId){
+        try{
+            return new ResponseEntity<>(HttpStatus.OK);
+        }catch (Exception e){
             logger.error(e.getMessage());
             return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
         }
