4d929718050d035333974e195c310037e3945213
Erik Csepeti <Erik-EEI@users.noreply.github.com>
2023-11-28 22:38:38 +0100

    Import new component wireframe into track page, create placeholder HTML

--- frontend/src/components/track/Savings/Savings.component.jsx
+++ frontend/src/components/track/Savings/Savings.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Savings.styles.css';
 
 const SavingsComponent = () => {
     return (
         <div className={"savings-page-overview"}>
-
+            <div className={"savings-actual-balance"}>Actual</div>
+            <div className={"savings-arrow-button-container"}>ARROWS</div>
+            <div className={"savings-saved-balance"}>Saved</div>
         </div>
     );
 };
