95671da27aa387aba7559d7ba86cd718465904b1
tmsnvkcc <127792799+tmsnvkcc@users.noreply.github.com>
2023-11-29 11:11:37 +0100

    add categories table to db; fix connection issues;

---
+++ backend/src/main/java/com/codecool/entity/TransactionCategory.java
@@ -0,0 +1,36 @@
+package com.codecool.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "transaction_categories")
+public class TransactionCategory {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private int id;
+
+  @Column(name = "name")
+  private String name;
+
+  public TransactionCategory() {}
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
